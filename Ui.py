053d009fb594ca531465661bbfcdabd27567c84b
unknown <yavrosman@gmail.com>
2024-04-28 14:13:03 +0300

    df

--- Ui.py
+++ Ui.py
@@ -1,27 +1,32 @@
 import tkinter as tk
 from tkinter import Frame, Label, Button, Text
 
-# Set up dark theme colors
-bg = '#333'  # dark gray
-txt_bg = '#444'  # lighter gray
-btn_bg_active = '#fff'  # button active color (green)
-btn_bg_inactive = '#ff0'  # button inactive color (yellow)
 
-root = tk.Tk()
-root.title('POE Helper Tool')
-root.configure(bg=bg)
+def create_gui():
+    root = tk.Tk()
+    root.title('POE Helper Tool')
 
-menu_panel = Frame(root, bg='#ff0', height=500, width=200)
-menu_panel.pack(side='left', pady=10)
+    # Set up dark theme colors
+    bg = '#333'  # dark gray
+    txt_bg = '#444'  # lighter gray
+    btn_bg_active = '#fff'  # button active color (green)
+    btn_bg_inactive = '#ff0'  # button inactive color (yellow)
 
-home_btn = Button(menu_panel, text='Home', bg=btn_bg_active, command=lambda: home_btn.config(bg=btn_bg_inactive))
-home_btn.pack(side='top', pady=5)
+    root.configure(bg=bg)
 
-close_btn = Button(menu_panel, text='Close', bg='red', command=root.destroy)
-close_btn.pack(side='bottom', pady=5)
+    menu_panel = Frame(root, bg='#ff0', height=500, width=200)
+    menu_panel.pack(side='left', pady=10)
 
-main_screen = Frame(root, bg='black', width=400)
-main_screen.pack(side='left', pady=10)
+    home_btn = Button(menu_panel, text='Home', bg=btn_bg_active, command=lambda: home_btn.config(bg=btn_bg_inactive))
+    home_btn.pack(side='top', pady=5)
 
-logger_panel = Text(root, bg=txt_bg, height=500, width=150)
-logger_panel.pack(side='right', pady=10)
+    close_btn = Button(menu_panel, text='Close', bg='red', command=root.destroy)
+    close_btn.pack(side='bottom', pady=5)
+
+    main_screen = Frame(root, bg='black', width=400)
+    main_screen.pack(side='left', pady=10)
+
+    logger_panel = Text(root, bg=txt_bg, height=500, width=150)
+    logger_panel.pack(side='right', pady=10)
+
+    root.mainloop()
