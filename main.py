feac07439473346fc07f9b73f591b756c82a5fc5
yavrosman <77438089+yavrosman@users.noreply.github.com>
2024-04-28 13:40:03 +0300

    Update main.py with basic GUI setup using PySimpleGUI

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-# Placeholder for main python application launcher
+#import PySimpleGUI as sg
 def main():
-    print('Hello, World!')
+    layout = [[
+        sg.Text('Hello World!'),
+        sg.Button('OK')
+    ]]
+    window = sg.Window('POE Helper Tool', layout)
+    while True:
+        if sg.window_read(window)[0] == 'EXIT':
+            break
+        else:
+            window.close()
 
 #if __name__ == '__main__':
     main()
